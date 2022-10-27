fe73e1008a361be891ec81c90d2fa2eed5a14586
nnodim <nnodimobinna@gmail.com>
2022-10-27 02:01:01 +0100

    Added models and attributed

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -1,3 +1,10 @@
+from re import A
 from django.contrib import admin
-
+from .models import Artiste, Song, Lyric
 # Register your models here.
+
+@admin.register(Artiste)
+class ArtisteAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "age")
+admin.site.register(Song)
+admin.site.register(Lyric)
--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=400)
+    last_name = models.CharField(max_length=400)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.first_name
+
+class Song(models.Model):
+    title = models.CharField(max_length=400)
+    date_released = models.DateField(default=datetime.now)
+    likes = models.CharField(max_length=400)
+    artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+class Lyric(models.Model):
+    content = models.CharField(max_length=400)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.content
